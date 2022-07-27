61717e5d59ea04cf1052a3fb92bfe5052effe824
Savkina Ekaterina <mykolesiko@mail.ru>
2022-07-27 18:48:14 +0300

    fixed bug

--- dops/functions.py
+++ dops/functions.py
@@ -31,7 +31,7 @@ def chunk(lst, n):
 
 
 def postprocess_text(text):
-   """Postprocess of text received from GPT-3 """
+    """Postprocess of text received from GPT-3 """
     text = text.replace('\n\n', '\n')
     words_list = text.split(" ")
     parts_list = list(chunk(words_list, 30))
@@ -152,7 +152,7 @@ def get_audio(sound_provider, text, voice, API_KEY, USER_ID):
 
 
 def get_images_tale(tale, title):
-    """Divide text into several parts and receive pictures for those parts """
+   """Divide text into several parts and receive pictures for those parts """
    summarizer = pipeline("summarization")
    sentences = tale.split(". ")
    n = len(sentences)
@@ -235,13 +235,6 @@ def get_sentiment(tale):
 
 def get_love_mood(tale):
     """Find bad words in text  """
-    tale_list = list(tale.split(" "))
-    root_words = []
-    ps = PorterStemmer()
-    for w in tale_list:
-         rootWord = ps.stem(w)
-         root_words.append(rootWord)
-
     for word in var_list:
         if tale.find(" " + word + " ") != -1:
             print(f"bad****{word}*****")
